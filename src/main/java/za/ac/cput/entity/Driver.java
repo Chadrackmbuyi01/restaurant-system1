6c78e6b4c2f4295a4016b2d44ec787b28b606590
ZubairE <esau.zubair98@gmail.com>
2022-04-06 16:14:31 +0200

    Creating Driver entity using builder pattern

--- src/main/java/za/ac/cput/entity/Driver.java
+++ src/main/java/za/ac/cput/entity/Driver.java
@@ -9,4 +9,109 @@ package za.ac.cput.entity;
 
 
 public class Driver {
+    private int DriverId ;
+    private int DeliveryId ;
+    private int OrderId ;
+    private String DriverName;
+
+    //private constructor
+    private Driver(Builder builder){
+        this.DriverId = builder.DriverId;
+        this.DeliveryId = builder.DeliveryId;
+        this.OrderId = builder.OrderId;
+        this.DriverName = builder.DriverName;
+    }
+
+    // Getters and setters
+
+
+    public int getDriverId() {
+        return DriverId;
+    }
+
+    public void setDriverId(int driverId) {
+        DriverId = driverId;
+    }
+
+    public int getDeliveryId() {
+        return DeliveryId;
+    }
+
+    public void setDeliveryId(int deliveryId) {
+        DeliveryId = deliveryId;
+    }
+
+    public int getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(int orderId) {
+        OrderId = orderId;
+    }
+
+    public String getDriverName() {
+        return DriverName;
+    }
+
+    public void setDriverName(String driverName) {
+        DriverName = driverName;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "DriverId=" + DriverId +
+                ", DeliveryId=" + DeliveryId +
+                ", OrderId=" + OrderId +
+                ", DriverName='" + DriverName + '\'' +
+                '}';
+    }
+
+    //Builder pattern class
+    public static class Builder {
+        private int DriverId ;
+        private int DeliveryId ;
+        private int OrderId ;
+        private String DriverName;
+
+        public Builder setDriverId(int driverId) {
+            this.DriverId = driverId;
+            return this;
+        }
+
+        public Builder setDeliveryId(int deliveryId) {
+            this.DeliveryId = deliveryId;
+            return this;
+        }
+
+        public Builder setOrderId(int orderId) {
+            this.OrderId = orderId;
+            return this;
+        }
+
+        public Builder setDriverName(String driverName) {
+            DriverName = driverName;
+            return this;
+        }
+
+
+        public Builder copy( Driver driver){
+            this.DriverId = DriverId;
+            this.DeliveryId = DeliveryId;
+            this.OrderId = OrderId;
+            this.DriverName = DriverName;
+            return this;
+        }
+
+
+        public Driver build(){
+            return new Driver(this);
+        }
+
+
+
+    }
+
+
 }
+
