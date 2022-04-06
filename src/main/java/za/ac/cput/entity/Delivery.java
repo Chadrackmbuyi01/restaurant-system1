bc334f32a9d18d18e8ca3fdb2dc1f35a8d7f507b
ZubairE <esau.zubair98@gmail.com>
2022-04-06 16:15:54 +0200

    Creating Delivery entity using builder pattern

--- src/main/java/za/ac/cput/entity/Delivery.java
+++ src/main/java/za/ac/cput/entity/Delivery.java
@@ -12,9 +12,64 @@ package za.ac.cput.entity;
 
 
 public class Delivery {
+    private int DeliveryId ;
+    private int OrderId ;
+
+    // private constructor
+    private Delivery (Builder builder){
+        this.DeliveryId = builder.DeliveryId;
+        this.OrderId = builder.OrderId;
+    }
 
 
+    //Getters and setters
 
 
+    public int getDeliveryId() {
+        return DeliveryId;
+    }
+
+    public void setDeliveryId(int deliveryId) {
+        DeliveryId = deliveryId;
+    }
+
+    public int getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(int orderId) {
+        OrderId = orderId;
+    }
+
+    //Builder pattern class
+    public static class Builder{
+        private int DeliveryId ;
+        private int OrderId ;
+
+
+        public Builder setDeliveryId(int deliveryId) {
+            this.DeliveryId = deliveryId;
+            return this;
+        }
+
+        public Builder setOrderId(int orderId) {
+            this.OrderId = orderId;
+            return this;
+        }
+
+
+        public Builder copy(Delivery delivery){
+            this.DeliveryId = DeliveryId;
+            this.OrderId = OrderId;
+            return this;
+        }
+
+        public Delivery build(){
+            return new Delivery(this);
+        }
+
+    }
 
 }
+
+
