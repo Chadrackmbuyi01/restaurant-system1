acde6c51afbdad9ff5bf40fb78ee0f56779c6cf4
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-04-06 23:07:25 +0200

    Created the employeefactory class and his test class. The test runs successfully

--- src/main/java/za/ac/cput/factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/factory/EmployeeFactory.java
@@ -1,4 +1,15 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Employee;
+
 public class EmployeeFactory {
+
+    public static Employee createEmployee(int empId, String empFname, String empLname, String empAddress){
+                    return new Employee.Builder().setEmpId(empId)
+                            .setEmpFname(empFname)
+                            .setEmpLname(empLname)
+                            .setEmpAddress(empAddress)
+                            .build();
+
+    }
 }
--- src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -1,7 +1,18 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Employee;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeFactoryTest {
+
+    @Test
+    public void test(){
+
+        Employee employee = EmployeeFactory.createEmployee(103, "Chadrack", "Kalala", "3 2nd Avenue, Rondebosch East");
+        assertNotNull(employee);
+
+    }
 
 }
