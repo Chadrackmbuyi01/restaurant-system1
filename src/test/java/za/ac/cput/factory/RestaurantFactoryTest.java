faf9dd2b3b5caec1002f2193006cbead6fa1aa10
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-04-06 23:17:35 +0200

    Creation of the restaurant factory test class, it has ran successfully

--- src/test/java/za/ac/cput/factory/RestaurantFactoryTest.java
+++ src/test/java/za/ac/cput/factory/RestaurantFactoryTest.java
@@ -1,7 +1,17 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Restaurant;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantFactoryTest {
 
+    @Test
+    public void test() {
+
+        Restaurant restaurant = RestaurantFactory.createRestaurant(1001545,"Slug and Lettuce", " Liesbeek Parkway 625, Observatory");
+        assertNotNull(restaurant);
+
+    }
 }
