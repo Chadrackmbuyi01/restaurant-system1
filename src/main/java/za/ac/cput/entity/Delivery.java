53187ab8bdadfa33e2306da25ca0f2fd87f65d43
ZubairE <esau.zubair98@gmail.com>
2022-04-06 16:12:37 +0200

    Adding comments to Driver and Delivery entity

---
+++ src/main/java/za/ac/cput/entity/Delivery.java
@@ -0,0 +1,20 @@
+/*
+Delivery.java
+Entity for delivery using builder pattern
+Author : Zubair Esau (217100554)
+Date 05 April 2022
+
+ */
+
+package za.ac.cput.entity;
+
+
+
+
+public class Delivery {
+
+
+
+
+
+}
---
+++ src/main/java/za/ac/cput/entity/Driver.java
@@ -0,0 +1,12 @@
+/*
+Driver.java
+Entity for driver using builder pattern
+Author : Zubair Esau (217100554)
+Date 05 April 2022
+
+ */
+package za.ac.cput.entity;
+
+
+public class Driver {
+}
