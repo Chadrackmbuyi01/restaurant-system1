3dff23cdcf0c296d523dd9506c27a6a25fc36d66
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-04-06 23:13:25 +0200

    Creation of restaurant factory class

--- src/main/java/za/ac/cput/factory/RestaurantFactory.java
+++ src/main/java/za/ac/cput/factory/RestaurantFactory.java
@@ -1,4 +1,14 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Employee;
+import za.ac.cput.entity.Restaurant;
+
 public class RestaurantFactory {
+
+    public static Restaurant createRestaurant(int restId, String restName, String restAddress) {
+        return new Restaurant.Builder().setRestId(restId)
+                .setRestName(restName)
+                .setRestAddress(restAddress)
+                .build();
+    }
 }
