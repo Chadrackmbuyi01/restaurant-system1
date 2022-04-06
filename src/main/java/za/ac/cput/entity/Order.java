7af9b521da74230bcc36b7dae005dacb3c629f7e
Ismail Watara <joeywatara17@gmail.com>
2022-04-06 16:01:49 +0200

    Committing Entities

---
+++ src/main/java/za/ac/cput/entity/Order.java
@@ -0,0 +1,63 @@
+package za.ac.cput.entity;
+
+public class Order {
+    private String orderId;
+    private String orderItem;
+
+
+    private Order(Builder builder){
+        this.orderId = builder.orderId;
+        this.orderItem = builder.orderItem;
+
+
+    }
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderItem() {
+        return orderItem;
+    }
+
+    public void setOrderItem(String orderItem) {
+        this.orderItem = orderItem;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderItem='" + orderItem + '\'' +
+                '}';
+
+    }
+
+    public static class Builder {
+        private String orderId;
+        private String orderItem;
+
+        public Builder setorderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder setorderItem(String orderItem) {
+            this.orderItem = orderItem;
+            return this;
+        }
+
+        public Builder copy(Order order){
+            this.orderId = order.orderId;
+            this.orderItem = order.orderItem;
+            return this;
+        }
+        public Order build(){
+            return new Order(this);
+        }
+    }
+
+}
