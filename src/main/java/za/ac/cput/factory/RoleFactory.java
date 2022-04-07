a438c53f642da8dad3961137645278ce378ca16a
Marcia Zanele Bika <211054356@mycput.ac.za>
2022-04-07 13:04:56 +0200

    added appropriate comment line

---
+++ src/main/java/za/ac/cput/factory/RoleFactory.java
@@ -0,0 +1,11 @@
+/* RoleFactory.java
+   Entity for EmployeeRole
+   Author: Marcia Zanele Bika (211054356)
+   Date: 03 April 2022
+ */
+
+package za.ac.cput.factory;
+
+public class RoleFactory {
+
+}
