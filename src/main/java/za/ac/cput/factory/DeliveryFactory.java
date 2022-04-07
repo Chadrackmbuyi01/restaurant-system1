f61b4265fa0d84d24c76958adf12b88bee61a2e8
ZubairE <esau.zubair98@gmail.com>
2022-04-07 14:27:41 +0200

    Creating Delivery Factory and Test for DeliveryFactory

--- src/main/java/za/ac/cput/factory/DeliveryFactory.java
+++ src/main/java/za/ac/cput/factory/DeliveryFactory.java
@@ -6,10 +6,21 @@ Date 06 April 2022
 
  */
 
-
-
-
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Delivery;
+import za.ac.cput.entity.Driver;
+
+import java.util.UUID;
+
 public class DeliveryFactory {
+
+    public static Delivery createDelivery(String deliveryId, String orderId){
+        return new Delivery.Builder().setDeliveryId(Integer.parseInt(deliveryId))
+                .setOrderId(Integer.parseInt(orderId))
+                .build();
+
+
+    }
+
 }
