677de4ba9fad726bf15e92b9c54431871461f633
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-04-07 17:34:50 +0200

    Added code to PaymentFactory and PaymentFactoryTest

--- src/main/java/za/ac/cput/factory/PaymentFactory.java
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -1,4 +1,13 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Payment;
+
 public class PaymentFactory {
+    public static Payment createPayment(String paymentId, String payCash, String payCard, String payEft) {
+        return new Payment.Builder().setPaymentId(Integer.parseInt(paymentId))
+                .setPayCash(payCash)
+                .setPayCard(payCard)
+                .setPayEft(payEft)
+                .build();
+    }
 }
--- src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
@@ -1,4 +1,15 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Payment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PaymentFactoryTest {
+
+    @Test
+    public void test() {
+        Payment payment = PaymentFactory.createPayment("350", "yes", "no", "no");
+        assertNotNull(payment);
+    }
 }
