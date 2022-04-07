98349db5536a7a70e8cd6332583f55b0b5c60caf
ZubairE <esau.zubair98@gmail.com>
2022-04-07 13:54:00 +0200

    Adding comments to Driver and Delivery Factory

---
+++ src/main/java/za/ac/cput/factory/DeliveryFactory.java
@@ -0,0 +1,15 @@
+/*
+DeliveryFactory.java
+Delivery Factory for delivery Entity
+Author : Zubair Esau (217100554)
+Date 06 April 2022
+
+ */
+
+
+
+
+package za.ac.cput.factory;
+
+public class DeliveryFactory {
+}
---
+++ src/main/java/za/ac/cput/factory/DriverFactory.java
@@ -0,0 +1,15 @@
+/*
+DriverFactory.java
+Driver Factory for driver Entity
+Author : Zubair Esau (217100554)
+Date 06 April 2022
+
+ */
+
+
+
+
+package za.ac.cput.factory;
+
+public class DriverFactory {
+}
