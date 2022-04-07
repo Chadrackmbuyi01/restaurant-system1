eb393aa8f71e7e843292e7b98506b072a138a561
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-04-07 17:08:32 +0200

    Added code to MenuFactory and MenuFactoryTest

--- src/main/java/za/ac/cput/factory/MenuFactory.java
+++ src/main/java/za/ac/cput/factory/MenuFactory.java
@@ -1,4 +1,14 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Menu;
+
+import java.util.UUID;
+
 public class MenuFactory {
+    public static Menu createMenu(String menuId, String menuItem) {
+        return new Menu.Builder().setMenuId(Integer.parseInt(menuId))
+                .setMenuItem(menuItem)
+                .build();
+    }
+
 }
--- src/test/java/za/ac/cput/factory/MenuFactoryTest.java
+++ src/test/java/za/ac/cput/factory/MenuFactoryTest.java
@@ -1,4 +1,17 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MenuFactoryTest {
+
+    @Test
+    public void test() {
+        Menu menu = MenuFactory.createMenu("52", "Chips");
+        assertNotNull(menu);
+    }
+
+
 }
