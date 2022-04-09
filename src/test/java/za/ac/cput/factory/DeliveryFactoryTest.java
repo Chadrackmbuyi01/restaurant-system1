a32672dd35cad64735bed3c5b9eed69e66490f7f
ZubairE <esau.zubair98@gmail.com>
2022-04-09 12:52:19 +0200

    Adding new comments to Driver and delivery Factory test

--- src/test/java/za/ac/cput/factory/DeliveryFactoryTest.java
+++ src/test/java/za/ac/cput/factory/DeliveryFactoryTest.java
@@ -1,3 +1,11 @@
+/*
+DriverFactoryTest.java
+Test class for Driver Factory
+Author : Zubair Esau (217100554)
+Date 06 April 2022
+
+ */
+
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
--- src/test/java/za/ac/cput/factory/DriverFactoryTest.java
+++ src/test/java/za/ac/cput/factory/DriverFactoryTest.java
@@ -1,3 +1,11 @@
+/*
+DeliveryFactoryTest.java
+Test class for Delivery Factory
+Author : Zubair Esau (217100554)
+Date 06 April 2022
+
+ */
+
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
