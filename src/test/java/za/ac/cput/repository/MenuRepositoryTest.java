e3148639aba809d85c6a07b937a689f18452a6f3
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-04-09 19:53:41 +0200

    Added Code to Menu Repository Test

--- src/test/java/za/ac/cput/repository/MenuRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/MenuRepositoryTest.java
@@ -1,4 +1,56 @@
 package za.ac.cput.repository;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.entity.Menu;
+import za.ac.cput.factory.MenuFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.MethodName.class)
+
 public class MenuRepositoryTest {
+
+    private static MenuRepository menuRepository = MenuRepository.getMenuRepository();
+    private static Menu menu = MenuFactory.createMenu("20", "Steak");
+
+    @Test
+    void a_create() {
+        Menu create = menuRepository.create(menu);
+        assertEquals(menu.getMenuId(), create.getMenuId());
+        System.out.println("Create: "+ create);
+    }
+
+    @Test
+    void b_read() {
+        Menu read = menuRepository.read(menu.getMenuId());
+        assertNotNull(read);
+        System.out.println("Read:"+ read);
+    }
+
+    @Test
+    void c_update() {
+
+        Menu update = new Menu.Builder().copy(menu).setMenuId("50").setMenuItem("Fish")
+                .build();
+        System.out.println("Update:"+ update);
+
+    }
+    @Test
+    void e_delete() {
+
+        boolean success = menuRepository.delete(menu.getMenuId());
+        assertTrue(success);
+        System.out.println("Has been deleted :"+ success);
+
+    }
+
+    @Test
+    void d_getAll() {
+
+        System.out.println( menuRepository.getAll());
+        System.out.println("Show all :");
+
+
+    }
 }
