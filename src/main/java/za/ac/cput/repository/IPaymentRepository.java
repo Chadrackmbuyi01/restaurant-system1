1fbc558a7db30d37a20cf00851e69fdc8ff11fbc
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-04-09 19:55:15 +0200

    Added Code to Payment Repository Interface

--- src/main/java/za/ac/cput/repository/IPaymentRepository.java
+++ src/main/java/za/ac/cput/repository/IPaymentRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.repository;
 
-public interface IPaymentRepository {
+import za.ac.cput.entity.Payment;
+import java.util.Set;
+
+public interface IPaymentRepository extends IRepository <Payment, String>{
+    public Set<Payment> getAll();
 }
