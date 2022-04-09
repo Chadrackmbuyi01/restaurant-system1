83dfb7d13054f158bf90a950897ea7a0c4a02773
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-04-09 15:07:35 +0200

    Defining the interface of the employee repository where to store method CRUD

--- src/main/java/za/ac/cput/repository/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/repository/IEmployeeRepository.java
@@ -1,4 +1,11 @@
 package za.ac.cput.repository;
 
-public interface IEmployeeRepository {
+import za.ac.cput.entity.Employee;
+
+import java.util.Set;
+
+public interface IEmployeeRepository extends IRepository<Employee, Integer> {
+    // The interface of the Employee Repo where I will define create, read, update and delete methods
+
+    public Set<Employee> getAll();
 }
