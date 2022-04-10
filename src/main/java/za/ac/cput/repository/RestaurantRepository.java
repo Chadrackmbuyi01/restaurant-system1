37440d0f70d6b23ecc011baeafdf73d45713336e
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-04-10 16:22:51 +0200

    Create and read operations have been added

--- src/main/java/za/ac/cput/repository/RestaurantRepository.java
+++ src/main/java/za/ac/cput/repository/RestaurantRepository.java
@@ -1,18 +1,41 @@
 package za.ac.cput.repository;
 
+import za.ac.cput.entity.Employee;
 import za.ac.cput.entity.Restaurant;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class RestaurantRepository implements IRestaurantRepository{
-    @Override
-    public Restaurant create(Restaurant restaurant) {
-        return null;
+    private static RestaurantRepository repo = null;
+    private Set<Restaurant> restaurantDataBase = null;
+
+    private RestaurantRepository(){
+        restaurantDataBase = new HashSet<Restaurant>();
+    }
+
+    public static RestaurantRepository getRepository(){
+        if(repo == null){
+            repo = new RestaurantRepository();
+        }
+        return repo;
     }
 
     @Override
-    public Restaurant read(String s) {
-        return null;
+    public Restaurant create(Restaurant restaurant) {
+        boolean success = restaurantDataBase.add(restaurant);
+        if (!success)
+            return null;
+        return restaurant;
+    }
+
+    @Override
+    public Restaurant read(String restId) {
+        Restaurant restaurant = restaurantDataBase.stream()
+                .filter(re -> re.getRestRegNumber().equals(restId))
+                .findAny()
+                .orElse(null);
+        return restaurant;
     }
 
     @Override
