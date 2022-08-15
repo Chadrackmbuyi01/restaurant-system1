88a9d79bcabe5c0cd241231c0e1735e629a55cbc
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-08-15 08:07:15 +0200

    Connected the application into JDBC mysql database and i fixed bugs into util package files

---
+++ src/main/java/za/ac/cput/myJDBC/Driver.java
@@ -0,0 +1,21 @@
+package za.ac.cput.myJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-connection", "root", "Chadsmith2022");
+
+            Statement statement = connection.createStatement();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
