6b33fc75947acee44f3bb0a1c50c1ab5eadb982f
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-08-21 15:51:51 +0200

    Creating the service interface package and implementation

--- src/main/java/za/ac/cput/myJDBC/Configuration.java
+++ src/main/java/za/ac/cput/myJDBC/Configuration.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class Driver {
+public class Configuration {
     public static void main(String[] args) {
 
         try {
